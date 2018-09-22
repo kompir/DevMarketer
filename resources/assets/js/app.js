ab84e52c17f4a4ccd5ee28d8a67f0fbf165e5e1d
aalexandyr <aalexandyr@gmail.com>
2018-09-22 18:56:32 +0300

    Configure JS and CSS and DB

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,14 +9,20 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+//import Vue from 'vue'
+import Buefy from 'buefy'
+//import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
